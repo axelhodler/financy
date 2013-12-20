5bda0a84e703860444b82bded35892a7753772ce
xorrr <derxor@gmail.com>
2013-12-20 17:49:30 +0100

    remove obsolete test

--- spec/FinanceSpec.js
+++ spec/FinanceSpec.js
@@ -5,16 +5,6 @@ describe("Finance", function() {
         finance = new Finance();
     });
 
-    describe("when accessing the Yahoo Finance API", function() {
-        it("should use an url encoded query", function() {
-            var query = 'select * from yahoo.finance.stocks ' +
-                    'where symbol="yhoo"';
-            var encodedQuery = 'select%20*%20from%20yahoo.finance.stocks' +
-                    '%20where%20symbol%3D%22yhoo%22';
-            expect(finance.encodeQuery(query)).toEqual(encodedQuery);
-        });
-    });
-
     describe("when looking for the Sector of the Yahoo stock", function() {
         it("should be 'Technology'", function() {
             stockObj = finance.getStockInfos("YHOO");
