8e5a89d64b22d6d086a12248938fbe677070939e
xorrr <derxor@gmail.com>
2013-12-20 18:29:10 +0100

    put the get data and draw chart into its own method and call from index.html

--- js/chart1.js
+++ js/chart1.js
@@ -1,23 +1,26 @@
-finance = new Finance();
+function getDataAndDrawChart(symbol, startDate, endDate) {
 
-DatePrices = finance.getHistoricalInfos("YHOO", "2012-01-03","2012-01-07");
+    finance = new Finance();
 
-Dates = DatePrices[0];
-Prices = DatePrices[1];
+    DatePrices = finance.getHistoricalInfos(symbol, startDate, endDate);
 
-var stockData = {
-    labels : Dates,
-    datasets : [
-        {
-            fillColor : "rgba(255,0,0,0.5)",
-            strokeColor : "#00ff00",
-            pointColor : "#fff",
-            pointStrokeColor : "#0000ff",
-            data : Prices
-        }
-    ]
+    Dates = DatePrices[0];
+    Prices = DatePrices[1];
+
+    var stockData = {
+        labels : Dates,
+        datasets : [
+            {
+                fillColor : "rgba(255,0,0,0.5)",
+                strokeColor : "#00ff00",
+                pointColor : "#fff",
+                pointStrokeColor : "#0000ff",
+                data : Prices
+            }
+        ]
+    };
+
+    var stock1 = document.getElementById('stock1').getContext('2d');
+
+    new Chart(stock1).Line(stockData);
 };
-
-var stock1 = document.getElementById('stock1').getContext('2d');
-
-new Chart(stock1).Line(stockData);
